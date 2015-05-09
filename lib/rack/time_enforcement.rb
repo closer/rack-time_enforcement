2b033742abe6c386f24a0125250839e47820fef6
closer <closer009@gmail.com>
2015-05-09 17:23:26 +0900

    initial commit

---
+++ lib/rack/time_enforcement.rb
@@ -0,0 +1,7 @@
+require "rack/time_enforcement/version"
+
+module Rack
+  module TimeEnforcement
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/time_enforcement/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module TimeEnforcement
+    VERSION = "0.1.0"
+  end
+end
